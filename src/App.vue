970767578e5bcc2d17e26ef698f54d62445bf69d
Desire4sorrow <56004655+Desire4sorrow@users.noreply.github.com>
2022-03-21 22:57:27 +0300

    added logo and main page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/Logo.png">
+  <HelloWorld msg="Saas Market Project"/>
 </template>
 
 <script>
