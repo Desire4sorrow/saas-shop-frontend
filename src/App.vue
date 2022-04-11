bb9e685ab5af653e700de7379c8e6e1253ab184f
Ирина Загайнова <ayrin.zagaynova@gmail.com>
2022-04-11 21:47:42 +0300

    Добавлена локальная авторизация

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <router-view></router-view>
+  <router-view :keycloaks=keycloak></router-view>
 </template>
 
 <script>
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Keycloak from 'keycloak-js'
 
 export default {
   name: 'App',
+  props: {
+      keycloak: Keycloak
+    },
+  mounted() {
+    fetch('https://localhost:5001/api/authtest/some-data')
+    .then(res => res.json())
+    .then(json => console.log(json));
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as Vue from 'vue'
 import * as Router from 'vue-router'
 import axios from 'axios';
 import VueAxios from 'vue-axios'
+import Keycloak from 'keycloak-js'
 
 import App from './App.vue'
 import {routes} from './router'
@@ -13,3 +14,43 @@ const router = Router.createRouter({
 })
 
 Vue.createApp(App).use(router, VueAxios, axios).mount('#app')
+
+let initOptions = {
+    url : 'http://localhost:8080',
+    realm : 'saas-shop',
+    clientId : 'saas-shop-client',
+    onLoad  : 'login-required',
+    checkLoginIframe: false
+  }
+  
+  let keycloak = Keycloak(initOptions);
+  
+keycloak.init({onLoad: initOptions.onLoad}).then( auth => {
+    if (!auth) {
+      window.location.reload();
+    } else {
+  
+      new Vue({
+        render: h => h(App, { props : {keycloak: keycloak}}),
+      }).$mount('#app')
+    }
+  
+     setInterval(() => {
+      keycloak.updateToken(70).then((refreshed) => {
+        if (refreshed) {
+          Vue.$log.info('Token refreshed' + refreshed);
+        } else {
+          Vue.$log.warn('Token not refreshed, valid for '
+            + Math.round(keycloak.tokenParsed.exp
+            + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+        }
+      }).catch(() => {
+        Vue.$log.error('Failed to refresh token');
+      });
+    }, 6000)
+  
+  }).catch(() => {
+    Vue.$log.error("Authenticated Failed");
+  });
+  
+  Vue.prototype.$keycloak = keycloak
