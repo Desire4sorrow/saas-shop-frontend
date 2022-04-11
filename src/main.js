54b71b76d5c01053d50c6ca8b3db64a0d721eef6
Ирина Загайнова <ayrin.zagaynova@gmail.com>
2022-04-11 23:24:03 +0300

    Добавлена keycloack авторизация

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const router = Router.createRouter({
 Vue.createApp(App).use(router, VueAxios, axios).mount('#app')
 
 let initOptions = {
-    url : 'http://localhost:8080',
+    url : 'https://testvm.plotpad.ru:8443/',
     realm : 'saas-shop',
     clientId : 'saas-shop-client',
     onLoad  : 'login-required',
