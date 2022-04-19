c059ffebbc6398007de47dddc69d2e99145fc2ae
chicony2753 <chicony2753@gmail.com>
2022-04-19 21:33:45 +0300

    Update config v2

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const router = Router.createRouter({
 Vue.createApp(App).use(router, VueAxios, axios).mount('#app')
 
 let initOptions = {
-    url : (location.host === 'testvm.plotpad.ru') ? 'https://testvm.plotpad.ru:8443/' : 'http://localhost:8080/',
+    url : 'https://testvm.plotpad.ru:8443/',
     realm : 'saas-shop',
     clientId : 'saas-shop-client',
     onLoad  : 'login-required',
