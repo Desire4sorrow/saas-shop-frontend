5abf08401102605a9dac34a87b2cd3a665297bd7
chicony2753 <chicony2753@gmail.com>
2022-04-20 19:56:36 +0300

    Change vue config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 4000,
-    proxy: (location.host === 'testvm.plotpad.ru') ? 'http://testvm.plotpad.ru:4964' : 'http://localhost:4964'
+    proxy: (process.env.NODE_ENV === 'production') ? 'http://testvm.plotpad.ru:4964' : 'http://localhost:4964'
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/account/' : '/'
 })
