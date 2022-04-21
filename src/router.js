0ad6430088ef58099aac7586ecc4b81cda28101f
chicony2753 <chicony2753@gmail.com>
2022-04-22 00:08:49 +0300

    Mb fix

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ import NotFound from "@/components/NotFound";
 
 export const routes = [
     {
-        path: '/',
+        path: '/*',
         name: 'BodyPage',
         component: BodyPage,
         meta: {
